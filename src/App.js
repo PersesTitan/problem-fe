9df8f93ef673675a50431a9f8fef089463fde0b2
PersesTitan <torampersesTitan@gmail.com>
2023-01-08 23:51:47 +0900

    shot footer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import {BrowserRouter, Link, Routes, Route} from "react-router-dom";
 import Login from "./router/Login";
 import Main from "./router/Main"
+import FooterMake from "./footer/FooterMake";
 
 function App() {
-  return (
+    return (
     <BrowserRouter>
       <div className="App">
           <header className="App-header">
@@ -21,19 +22,8 @@ function App() {
           </body>
 
           <footer className="App-footer">
-              <tr>
-                  <th>제작</th>
-                  <td><a href="https://github.com/PersesTitan">PersesTitan</a></td>
-              </tr>
-              <tr>
-                  <th>프로젝트</th>
-                  <td>
-                      <a href="https://github.com/Shabi-Project">
-                      Shabi
-                      <img src="https://avatars.githubusercontent.com/Shabi-project" alt="/"/>
-                      </a>
-                  </td>
-              </tr>
+              {FooterMake.footerItemMake("제작", "https://github.com/PersesTitan", "PersesTitan")}
+              {FooterMake.footerItemMake("프로젝트", "https://github.com/Shabi-Project", "Shabi", "https://avatars.githubusercontent.com/Shabi-project")}
           </footer>
       </div>
     </BrowserRouter>
