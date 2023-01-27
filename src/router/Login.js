57ed00f987f8d1a106fa169153938aa531dfa429
PersesTitan <torampersesTitan@gmail.com>
2023-01-27 15:06:03 +0900

    create Login

--- src/router/Login.js
+++ src/router/Login.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import {GithubLoginButton, GoogleLoginButton} from "react-social-login-buttons";
+import axios from "axios";
 
 const Login = (props) => {
+    const [a, setA] = useState("");
+    useEffect(() => {
+        axios('/api/login/github')
+            .then(res => {
+                setA(res.data)
+            });
+    },[])
+
     return (
-        <></>
+        <div className={"login-icons"}>
+            <GithubLoginButton onClick={() => axios.get("/api/login?service=github")}>
+                <span>Github</span>
+            </GithubLoginButton>
+            <GoogleLoginButton onClick={() => window.open('/api/login/google')}>
+                <span>Google</span>
+            </GoogleLoginButton>
+        </div>
     )
 }
 
